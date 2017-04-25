bb23896c6a2d8f60086c2ddc10c5018c9ee81cf2
Maximilian Goldschmidt <maxigoldschmidt@gmail.com>
2017-04-25 21:43:11 +0200

    Install Script Update

--- MaeCAN-Server/installmaecanserver.sh
+++ MaeCAN-Server/installmaecanserver.sh
@@ -1,8 +1,21 @@
 #! /bin/bash
-sudo apt-get install nodejs apache2 php5
+
+echo 'Installing needed packages ...'
+apt-get install apache2 php5 nodejs nodejs-legacy npm
+echo 'Setting up node server ...'
 rm /var/www/html/index.html
-mv -R ./* /var/www/ 
-rm /var/www/installmaecanserver.sh
+mv -R ./html /var/www/ 
+mv -R ./node /var/www/
 cd /var/www/node/
 npm install websocket ini
+echo 'Updating device tree for CAN ...'
+cd /boot/dtb
+mv sun7i-a20-bananapi.dtb sun7i-a20-bananapi.dtb_org
+wget lnxpps.de/bpi/bin/sun7i-a20-bananapi.dtb
+echo 'Creating can2lan and can2udp ...'
+cd /tmp/
+git clone --depth 1 https://github.com/GBert/railroad.git
+cd railroad/can2udp/src/
+make
+echo 'Done'
 
