7b19e53907e73538eceec1b86c5025872202c5eb
Maximilian Goldschmidt <maxigoldschmidt@gmail.com>
2017-04-27 01:21:08 +0200

    Update installmaecanserver.sh

--- MaeCAN-Server/installmaecanserver.sh
+++ MaeCAN-Server/installmaecanserver.sh
@@ -1,17 +1,10 @@
 #! /bin/bash
 
 sudo su
-echo -e '\033[0;34m Updating ... \033[0m'
-apt-get update
 echo -e '\033[0;34m Setting up node server ...\033[0m'
-cd /tmp/
-mkdir server/
-cd server/
+cd /var/www/
 wget https://github.com/Ixam97/MaeCAN-Project/raw/master/MaeCAN-Server/MaeCAN-Server.zip
 unzip MaeCAN-Server.zip
-rm /var/www/html/index.html
-cp -R html/ /var/www/ 
-cp -R node/ /var/www/
 cd /var/www/node/
 npm install websocket ini
 echo -e '\033[0;34m Updating device tree for CAN ...\033[0m'
@@ -25,6 +18,6 @@ cd railroad/can2udp/src/
 make
 cp can2udp /usr/bin/
 cp can2lan /usr/bin/
-echo -e '\033[0;32m Done!\033[0m \n Please Run "curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -" \n        and "apt-get install apache2 php libapache2-mod-php7.0 nodejs"'
+echo -e '\033[0;32m Done!\033[0m'
 
 exit 0
