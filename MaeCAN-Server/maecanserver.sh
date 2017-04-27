5f76382c8e871aec468f24eb449bda81cd36245a
Maximilian Goldschmidt <maxigoldschmidt@gmail.com>
2017-04-27 14:02:08 +0200

    Update maecanserver.sh

--- MaeCAN-Server/maecanserver.sh
+++ MaeCAN-Server/maecanserver.sh
@@ -5,4 +5,4 @@ ip link set can0 up type can bitrate 250000 restart-ms 10
 can2udp -d 15733 -l 15734 -b 127.0.0.1
 can2lan -c /var/www/html/config/ -m
 cd /var/www/node
-nodejs maecanserver.js
+node maecanserver.js
