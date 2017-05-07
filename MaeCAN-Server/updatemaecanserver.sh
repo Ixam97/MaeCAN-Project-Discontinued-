9979924576703db2599e3f504f2f34c18578dd0e
Maximilian Goldschmidt <maxigoldschmidt@gmail.com>
2017-05-07 21:14:36 +0200

    Update updatemaecanserver.sh

--- MaeCAN-Server/updatemaecanserver.sh
+++ MaeCAN-Server/updatemaecanserver.sh
@@ -1,8 +1,12 @@
 mv /var/www/html/config/ /tmp/config/
 cd /var/www/
+rm -R ./node/
+rm -R ./html/
 wget https://github.com/Ixam97/MaeCAN-Project/raw/master/MaeCAN-Server/MaeCAN-Server.zip
 unzip MaeCAN-Server.zip
 mv /tmp/config/ /var/www/html/config/
+cd ./node/
+npm install ini websocket
 cd /etc/init.d/
 wget https://raw.githubusercontent.com/Ixam97/MaeCAN-Project/master/MaeCAN-Server/maecanserver.sh
 chmod +x maecanserver.sh
